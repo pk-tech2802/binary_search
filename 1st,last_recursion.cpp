1aa439486b99d621708083c128fc727a2b3e6e3d
pk-tech2802 <75025527+pk-tech2802@users.noreply.github.com>
2022-04-29 22:36:52 +0530

    Update 1st,last_recursion.cpp

--- 1st,last_recursion.cpp
+++ 1st,last_recursion.cpp
@@ -3,24 +3,12 @@ using namespace std;
 void ser(vector<int> v, int s, int e, int &s1, int &e1, int t)
 {
     int mid = s + (e - s) / 2;
-    if (s == e && v[s] == t && s1 != -1 && e1 != -1)
-    {
-        if (s < s1)
-        {
-            s1 = s;
-        }
-        else
-        {
-            e1 = s;
-        }
-        return;
-    }
-
+    
     if (s <= e)
     {
         if (v[mid] == t)
         {
-            s1 = mid, e1 = mid;
+            s1 = min(s1,mid), e1 = max(e1,mid);
             ser(v, s, mid - 1, s1, e1, t);
             ser(v, mid + 1, e, s1, e1, t);
             return;
@@ -45,7 +33,7 @@ int main()
     vector<int> v;
     v = {1, 2, 2, 3, 3, 4, 5};
 
-    int s = 0, e = 6, t = 5, s1 = -1, e1 = -1;
+    int s = 0, e = 6, t = 5, s1 = INT_MAX, e1 = INT_MIN;
     ser(v, s, e, s1, e1, t);
     cout
         << s1 << " " << e1 << "\n";
